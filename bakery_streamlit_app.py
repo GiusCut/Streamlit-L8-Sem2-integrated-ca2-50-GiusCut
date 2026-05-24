203f814fa607213a22bdc52a7df96caf9e40af4d
GiusCut <sba24030@student.cct.ie>
2026-05-24 18:11:30 +0100

    Update bakery_streamlit_app.py

--- bakery_streamlit_app.py
+++ bakery_streamlit_app.py
@@ -133,9 +133,9 @@ fig_top_items.update_layout(
     yaxis_title="Item",
     yaxis={"categoryorder": "total ascending"},
     xaxis={"range": [0, item_counts["Count"].max() * 1.20]},
-    plot_bgcolor=colors["background"],
-    paper_bgcolor=colors["background"],
-    font_color=colors["text"],
+    plot_bgcolor=colors["linen"],
+    paper_bgcolor=colors["linen"],
+    font_color=colors["darkslategray"],
 )
 
 fig_top_items.update_traces(
@@ -208,9 +208,9 @@ fig_monthly.update_layout(
     yaxis_title="Item",
     yaxis={"categoryorder": "total ascending"},
     xaxis={"range": [0, max_count * 1.30]},
-    plot_bgcolor=colors["background"],
-    paper_bgcolor=colors["background"],
-    font_color=colors["text"],
+    plot_bgcolor=colors["linen"],
+    paper_bgcolor=colors["linen"],
+    font_color=colors["darkslategray"],
     margin={"l": 160, "r": 140, "t": 90, "b": 90},
     transition={"duration": 800},
 )
@@ -296,9 +296,9 @@ fig_bubble.update_layout(
     title_font_size=text_size + 6,
     xaxis_title="Hour of day",
     yaxis_title="Item",
-    plot_bgcolor=colors["background"],
-    paper_bgcolor=colors["background"],
-    font_color=colors["text"],
+    plot_bgcolor=colors["linen"],
+    paper_bgcolor=colors["linen"],
+    font_color=colors["darkslategray"],
     margin={"l": 180, "r": 120, "t": 90, "b": 90},
     xaxis={
         "tickmode": "array",
